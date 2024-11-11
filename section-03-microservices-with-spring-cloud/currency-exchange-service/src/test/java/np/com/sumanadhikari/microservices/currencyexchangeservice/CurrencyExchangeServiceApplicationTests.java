3d57519bef128fee8f60f0945ad6235dc783510c
Suman Adhikari <expertxkid@gmail.com>
2024-11-11 11:50:52 -0600

    currency-exchange-service: init project

---
+++ section-03-microservices-with-spring-cloud/currency-exchange-service/src/test/java/np/com/sumanadhikari/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.sumanadhikari.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
