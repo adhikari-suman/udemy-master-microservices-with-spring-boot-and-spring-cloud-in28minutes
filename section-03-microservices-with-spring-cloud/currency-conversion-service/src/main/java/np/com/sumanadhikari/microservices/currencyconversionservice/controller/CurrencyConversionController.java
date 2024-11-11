629f8f43ac189b7115cb3a7730eb0a9e5470d38b
Suman Adhikari <expertxkid@gmail.com>
2024-11-11 15:23:20 -0600

    currency-conversion-service: fetch the response from currency-exchange-service for fetching currency exchange details

--- section-03-microservices-with-spring-cloud/currency-conversion-service/src/main/java/np/com/sumanadhikari/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ section-03-microservices-with-spring-cloud/currency-conversion-service/src/main/java/np/com/sumanadhikari/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,11 +1,14 @@
 package np.com.sumanadhikari.microservices.currencyconversionservice.controller;
 
 import np.com.sumanadhikari.microservices.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -16,6 +19,26 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal quantity
                                                          ){
-        return new CurrencyConversion(10001L,from,to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        String url = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(url,
+                                                         CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        if (currencyConversion == null) {
+            throw  new RuntimeException("Could not get currency conversion");
+        }
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                                      from, to, quantity,
+                                      currencyConversion.getConversionMultiple(),
+                                      quantity.multiply(currencyConversion.getConversionMultiple()),
+                                      currencyConversion.getEnvironment());
     }
 }
