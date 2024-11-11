ff77b88751dad797c5ed396cfc77ac3551c2f7d3
Suman Adhikari <expertxkid@gmail.com>
2024-11-11 12:18:19 -0600

    currency-exchange-service: add environment information in response

--- section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/bean/CurrencyExchange.java
+++ section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/bean/CurrencyExchange.java
@@ -8,6 +8,8 @@ public class CurrencyExchange {
     private String     to;
     private BigDecimal conversionMultiple;
 
+    private String environment;
+
     public CurrencyExchange() {
     }
 
@@ -48,5 +50,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 }
