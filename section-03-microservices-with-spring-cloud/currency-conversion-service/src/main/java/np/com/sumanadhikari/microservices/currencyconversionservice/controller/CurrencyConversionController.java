d2eb7bde1a42e8c944534e2ab367c9f04ee224e9
Suman Adhikari <expertxkid@gmail.com>
2024-11-12 08:00:22 -0600

    currency-conversion-service: add CurrencyConversion data using feign client

--- section-03-microservices-with-spring-cloud/currency-conversion-service/src/main/java/np/com/sumanadhikari/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ section-03-microservices-with-spring-cloud/currency-conversion-service/src/main/java/np/com/sumanadhikari/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package np.com.sumanadhikari.microservices.currencyconversionservice.controller;
 
 import np.com.sumanadhikari.microservices.currencyconversionservice.bean.CurrencyConversion;
+import np.com.sumanadhikari.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,28 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+                                                         ){
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        if (currencyConversion == null) {
+            throw  new RuntimeException("Could not get currency conversion");
+        }
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                                      from, to, quantity,
+                                      currencyConversion.getConversionMultiple(),
+                                      quantity.multiply(currencyConversion.getConversionMultiple()),
+                                      currencyConversion.getEnvironment() + " "+"feign");
+    }
+
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -39,6 +63,6 @@ public class CurrencyConversionController {
                                       from, to, quantity,
                                       currencyConversion.getConversionMultiple(),
                                       quantity.multiply(currencyConversion.getConversionMultiple()),
-                                      currencyConversion.getEnvironment());
+                                      currencyConversion.getEnvironment()+" "+"rest template");
     }
 }
