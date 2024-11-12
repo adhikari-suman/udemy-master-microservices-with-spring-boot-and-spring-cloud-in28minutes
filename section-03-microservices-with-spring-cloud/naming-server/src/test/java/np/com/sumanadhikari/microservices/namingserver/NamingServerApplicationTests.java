019ba2399d7f32cfe98394871dd238262d31e9b3
Suman Adhikari <expertxkid@gmail.com>
2024-11-12 08:01:12 -0600

    naming-server: init naming-server

---
+++ section-03-microservices-with-spring-cloud/naming-server/src/test/java/np/com/sumanadhikari/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.sumanadhikari.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
