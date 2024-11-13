5aa060d2061420819861bca075e269abff29617a
Suman Adhikari <expertxkid@gmail.com>
2024-11-13 11:38:01 -0600

    currency-exchange-service: add RateLimiter for limiting requests and BulkHead for limiting concurrent requests

--- section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,12 +1,10 @@
 package np.com.sumanadhikari.microservices.currencyexchangeservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -16,14 +14,17 @@ public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+//    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sampleApi(){
         logger.info("Sample API call received");
-
-        ResponseEntity<String> response = new RestTemplate()
-                .getForEntity("http://localhost:8080/some-dummy-url", String.class);
-
-        return response.getBody();
+//
+//        ResponseEntity<String> response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//
+//        return response.getBody();
+        return "sample-api";
     }
 
     public String hardCodedResponse(Exception ex){
