07dfd12695735f066515e313861908131bbc2165
Suman Adhikari <expertxkid@gmail.com>
2024-11-13 11:22:57 -0600

    currency-exchange-service: add CircuitBreaker for sample-api

--- section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ section-03-microservices-with-spring-cloud/currency-exchange-service/src/main/java/np/com/sumanadhikari/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,12 +1,12 @@
 package np.com.sumanadhikari.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import io.github.resilience4j.retry.annotation.Retry;
 
 
 @RestController
@@ -15,7 +15,8 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
     public String sampleApi(){
         logger.info("Sample API call received");
 
